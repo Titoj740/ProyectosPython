fae8d1e59b4ef628e9722957f3e4352043a37e19
Tito Jaramillo <titoj740@gmail.com>
2022-04-15 16:21:22 -0500

    Version funcional de numChino.py: futuras mejoras en optimizacion

--- numChino.py
+++ numChino.py
@@ -1,47 +1,95 @@
 import random
-
-def generador():
-    a = random.randint(10000,999999999)
-    return a
+from traceback import format_exception
 
 def numEnChino (a):
+    numerosChino = ('零','一','二','三','四','五','六','七','八','九','九')
     resultado = ""
     if a >= 10000000:
-        resultado = str(int(a/10000000)); "千" , queNumEs(int(a/10000000))
+        resultado = str(numerosChino[int(a/10000000)]) + '千'
         a = a%10000000
-        resultado = resultado, str(int(a/1000000)), "百" , queNumEs(int(a/1000000))
+        resultado = resultado + str(numerosChino[int(a/1000000)]) + '百'
         a = a%1000000
-        resultado = resultado, str(int(a/100000)), "十" , queNumEs(int(a/100000))
+        resultado = resultado + str(numerosChino[int(a/100000)]) + '十' 
         a = a%100000
-        resultado = resultado, str(int(a/10000)), "万" , queNumEs(int(a/10000))
-
-
-    elif a > 1000000:
-        resultado = resultado, str(int(a/1000000)), "百" , queNumEs(int(a/1000000))
+        resultado = resultado + str(numerosChino[int(a/10000)]) + '万'
+        a = a%10000
+        resultado = resultado + str(numerosChino[int(a/1000)]) + '千'
+        a = a%1000
+        resultado = resultado + str(numerosChino[int(a/100)]) + '百'
+        a = a%100
+        resultado = resultado + str(numerosChino[int(a/10)]) + '十'
+        a = a%10
+        resultado = resultado + str(numerosChino[int(a/1)]) + '个'
+    elif a >= 1000000:
+        resultado = resultado + str(numerosChino[int(a/1000000)]) + '百'
         a = a%1000000
-        resultado = resultado, str(int(a/100000)), "十" , queNumEs(int(a/100000))
+        resultado = resultado + str(numerosChino[int(a/100000)]) + '十' 
         a = a%100000
-        resultado = resultado, str(int(a/10000)), "万" , queNumEs(int(a/10000))
+        resultado = resultado + str(numerosChino[int(a/10000)]) + '万'
+        a = a%10000
+        resultado = resultado + str(numerosChino[int(a/1000)]) + '千'
+        a = a%1000
+        resultado = resultado + str(numerosChino[int(a/100)]) + '百'
+        a = a%100
+        resultado = resultado + str(numerosChino[int(a/10)]) + '十'
+        a = a%10
+        resultado = resultado + str(numerosChino[int(a/1)]) + '个'
         
-    elif a > 100000:
-        resultado = resultado, str(int(a/100000)), "十" , queNumEs(int(a/100000))
+    elif a >= 100000:
+        resultado = resultado + str(numerosChino[int(a/100000)]) + '十' 
         a = a%100000
-        resultado = resultado, str(int(a/10000)), "万" , queNumEs(int(a/10000))
+        resultado = resultado + str(numerosChino[int(a/10000)]) + '万'
+        a = a%10000
+        resultado = resultado + str(numerosChino[int(a/1000)]) + '千'
+        a = a%1000
+        resultado = resultado + str(numerosChino[int(a/100)]) + '百'
+        a = a%100
+        resultado = resultado + str(numerosChino[int(a/10)]) + '十'
+        a = a%10
+        resultado = resultado + str(numerosChino[int(a/1)]) + '个'
+    elif a >= 10000:
+        resultado = resultado + str(numerosChino[int(a/10000)]) + '万'
+        a = a%10000
+        resultado = resultado + str(numerosChino[int(a/1000)]) + '千'
+        a = a%1000
+        resultado = resultado + str(numerosChino[int(a/100)]) + '百'
+        a = a%100
+        resultado = resultado + str(numerosChino[int(a/10)]) + '十'
+        a = a%10
+        resultado = resultado + str(numerosChino[int(a/1)]) + '个'
+    elif a >= 1000:
+        resultado = resultado + str(numerosChino[int(a/1000)]) + '千'
+        a = a%1000
+        resultado = resultado + str(numerosChino[int(a/100)]) + '百'
+        a = a%100
+        resultado = resultado + str(numerosChino[int(a/10)]) + '十'
+        a = a%10
+        resultado = resultado + str(numerosChino[int(a/1)]) + '个'
+    elif a >= 100:
+        resultado = resultado + str(numerosChino[int(a/100)]) + '百'
+        a = a%100
+        resultado = resultado + str(numerosChino[int(a/10)]) + '十'
+        a = a%10
+        resultado = resultado + str(numerosChino[int(a/1)]) + '个'
+    elif a >= 10:
+        resultado = resultado + str(numerosChino[int(a/10)]) + '十'
+        a = a%10
+        resultado = resultado + str(numerosChino[int(a/1)]) + '个'    
     else:
-        resultado = resultado, str(int(a/10000)), "万" , queNumEs(int(a/10000))
+        resultado = resultado + str(numerosChino[int(a/1)]) + '个'
+
 
     return resultado
 
-
-def queNumEs (numA):
-    x = 1
-    contador = 0
-    numerosChino = ["零","一","二","三","四","五","六","七","八","九"]
-    while x != 0 or contador > 11 :
-        for i in numerosChino:
-            if numA == i:
-                x = 0
-                return numerosChino[i]
-        contador += 1
-
-print(numEnChino(generador()))
+print("Escriba un número mayor o igual que 0 y menor que 100000000")
+x = 1
+while(x!=0):
+    try:
+        a = int(input("Ingrese el valor\n"))
+        if a >= 0 and a < 100000000:
+            x = 0
+        else:
+            raise format_exception
+    except:
+        print("Escirba un número válido pendejooooo")
+print(numEnChino(a))
